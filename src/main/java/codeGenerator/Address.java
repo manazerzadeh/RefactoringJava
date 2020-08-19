de491a11550b0f6769b2d4af5f18e3ab299c19eb
manazerzadeh <manazerzadeh@yahoo.com>
2020-08-19 20:44:45 +0430

    self encapsulated fields

--- src/main/java/codeGenerator/Address.java
+++ src/main/java/codeGenerator/Address.java
@@ -4,19 +4,44 @@ package codegenerator;
  * Created by mohammad hosein on 6/28/2015.
  */
 public class Address {
-    public int num;
-    public TypeAddress type;
-    public VarType varType;
 
-    public Address(int num, VarType varType, TypeAddress type) {
+    public int getNum() {
+        return num;
+    }
+
+    public TypeAddress getType() {
+        return type;
+    }
+
+    public VarType getVarType() {
+        return varType;
+    }
+
+    public void setNum(int num) {
         this.num = num;
+    }
+
+    public void setType(TypeAddress type) {
         this.type = type;
+    }
+
+    public void setVarType(VarType varType) {
         this.varType = varType;
     }
 
+    private int num;
+    private TypeAddress type;
+    private VarType varType;
+
+    public Address(int num, VarType varType, TypeAddress type) {
+        setNum(num);
+        setType(type);
+        setVarType(varType);
+    }
+
     public Address(int num, VarType varType) {
-        this.num = num;
-        this.type = TypeAddress.Direct;
-        this.varType = varType;
+        setNum(num);
+        setType(TypeAddress.Direct);
+        setVarType(varType);
     }
 }
